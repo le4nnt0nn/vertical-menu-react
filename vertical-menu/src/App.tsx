caeb8abf4accbf50686a4fc6ba251470a8e74efc
Antonio Cortés <ditacj78@gmail.com>
2021-10-12 23:00:09 +0200

    App.tsx : created items and card

--- vertical-menu/src/App.tsx
+++ vertical-menu/src/App.tsx
@@ -1,9 +1,27 @@
 import { SideBarMenu } from "./components/SideBarMenu";
+import { SideBarMenuCard, SideBarMenuItem } from "./types/types";
+import { FcAdvertising } from 'react-icons/fc';
 
 function App() {
+  const items:SideBarMenuItem[] = [
+    {
+      id: '1',
+      label: 'Hola',
+      icon : FcAdvertising,
+      url: '/',
+    }
+  ];
+
+  const card:SideBarMenuCard = {
+    id: 'card01',
+    displayName: 'Antonio Cortés',
+    title: 'Desarrollador',
+    photoUrl: '',
+    url: '/',
+  }
   return (
     <div>
-      <SideBarMenu/>
+      <SideBarMenu items={items} card={card}/>
     </div>
   );
 }
