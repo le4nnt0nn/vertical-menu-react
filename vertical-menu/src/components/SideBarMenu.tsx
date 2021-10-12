5922787dd55951315fcf2f591bf4f4da4bffb075
Antonio Cortés <ditacj78@gmail.com>
2021-10-12 23:00:37 +0200

    SideBarMenu.tsx : added VscMenu Button

--- vertical-menu/src/components/SideBarMenu.tsx
+++ vertical-menu/src/components/SideBarMenu.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { VscMenu } from 'react-icons/vsc';
 import { SideBarMenuCard, SideBarMenuItem } from "./types/types.d";
 import { classNames } from '../util/classes';
 interface SideBarMenuProps {
@@ -8,7 +9,13 @@ interface SideBarMenuProps {
 
 export function SideBarMenu({ items, card }: SideBarMenuProps) {
     const [isOpen, setIsOpen] = useState<boolean>(true);
-    return <div className={classNames('SideBarMenu', isOpen ? 'expanded' : 'collapsed')}>
-
+    return <div 
+        className={classNames('SideBarMenu', isOpen ? 'expanded' : 'collapsed')}
+    >
+        <div className='menuButton'>
+            <button>
+                <VscMenu/>
+            </button>
+        </div>
     </div>;
 }
