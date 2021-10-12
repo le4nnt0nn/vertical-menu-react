645d7cb5aefebee9ef3171c797acdaa6610e487b
Antonio Cortés <ditacj78@gmail.com>
2021-10-12 23:35:23 +0200

    SideBarMenuItemView.tsx : added full function

--- vertical-menu/src/components/SideBarMenuItemView.tsx
+++ vertical-menu/src/components/SideBarMenuItemView.tsx
@@ -1,3 +1,21 @@
-export default function SideBarMenuItemView(props: any) {
-    return <div>SideBarMenuItemView</div>
+import { SideBarMenuItem } from "../types/types.d";
+import { classNames } from '../util/classes';
+
+interface SideBarMenuItemViewProps {
+    item: SideBarMenuItem;
+    isOpen: boolean;
+}
+
+export default function SideBarMenuItemView({ item, isOpen }: SideBarMenuItemViewProps) {
+    return <div className='SideBarMenuItem'>
+        <a href={item.url}>
+            <div className={classNames('ItemContent', isOpen ? '' : 'collapsed')}>
+                <div className='icon'>
+                    <item.icon size='32' />
+                </div>
+                <span className='label'>{item.label}</span>
+            </div>
+        </a>
+        {!isOpen ? <div className='toolbox'>{item.label}</div> : '' }
+    </div>
 }
