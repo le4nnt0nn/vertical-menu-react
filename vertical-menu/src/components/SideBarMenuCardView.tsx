c426ea13773166941425f5155c10af193c9f27de
Antonio Cortés <ditacj78@gmail.com>
2021-10-13 08:57:41 +0200

    SideBarMenuCardView.tsx : added function and props

--- vertical-menu/src/components/SideBarMenuCardView.tsx
+++ vertical-menu/src/components/SideBarMenuCardView.tsx
@@ -1,3 +1,18 @@
-export default function SideBarMenuCardView(props: any) {
-    return <div>SideBarMenuCardView</div>
+import { SideBarMenuCard } from "../types/types.d";
+import { classNames } from '../util/classes';
+interface SideBarMenuCardViewProps {
+    card: SideBarMenuCard;
+    isOpen: boolean;
+}
+
+export default function SideBarMenuCardView({card, isOpen}: SideBarMenuCardViewProps) {
+    return <div className='SideBarMenuCard'>
+        <img className='profile' src={card.photoUrl} width='100%'/>
+        <div className={classNames('profileInfo', 
+        isOpen ? '' : 'collapsed')}>
+            <div className='name'>{card.displayName}</div>
+            <div className='title'>{card.title}</div>
+            <div className='url'><a href={card.url}>Ir al perfil</a></div>
+        </div>
+    </div>
 }
