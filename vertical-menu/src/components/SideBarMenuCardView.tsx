97d61dcc9759baa222e4e00ee13f7f6b4ef5c579
Antonio Cortés <ditacj78@gmail.com>
2021-10-13 10:29:20 +0200

    SideBarMenuCardView.tsx : added alt to img

--- vertical-menu/src/components/SideBarMenuCardView.tsx
+++ vertical-menu/src/components/SideBarMenuCardView.tsx
@@ -7,7 +7,7 @@ interface SideBarMenuCardViewProps {
 
 export default function SideBarMenuCardView({card, isOpen}: SideBarMenuCardViewProps) {
     return <div className='SideBarMenuCard'>
-        <img className='profile' src={card.photoUrl} width='100%'/>
+        <img className='profile' alt={card.displayName} src={card.photoUrl} width='100%'/>
         <div className={classNames('profileInfo', 
         isOpen ? '' : 'collapsed')}>
             <div className='name'>{card.displayName}</div>
